b459e6987d746d1a8ca78bcbeb0e4b98501d7cfe
Ville Savolainen <vsavolai@c1r6p7.hive.fi>
2024-07-15 15:30:30 +0300

    added null check to humanB class

--- cpp02/ex00/Fixed.cpp
+++ cpp02/ex00/Fixed.cpp
@@ -1,29 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   HumanB.hpp                                         :+:      :+:    :+:   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vsavolai <vsavolai@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/12 11:55:35 by vsavolai          #+#    #+#             */
-/*   Updated: 2024/07/15 14:15:21 by vsavolai         ###   ########.fr       */
+/*   Created: 2024/07/15 15:26:48 by vsavolai          #+#    #+#             */
+/*   Updated: 2024/07/15 15:26:49 by vsavolai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef HUMANB_HPP
-# define HUMANB_HPP
-
-# include "Weapon.hpp"
-# include <iostream>
-
-class HumanB {
-    public:
-        HumanB(std::string name);
-        void    attack();
-        void    setWeapon(Weapon &weapon);
-    private:
-        std::string name;
-        Weapon      *weapon;
-};
-
-#endif
