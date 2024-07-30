aa66c1776ddb05ce133fbbed659f1ce46bd67bbb
Ville Savolainen <vsavolai@c3r1p4.hive.fi>
2024-07-30 12:15:48 +0300

    begin cpp09

---
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vsavolai <vsavolai@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/30 08:36:02 by vsavolai          #+#    #+#             */
+/*   Updated: 2024/07/30 08:52:25 by vsavolai         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BITCOINEXCHANGE_HPP
+# define BITCOINEXCHANGE_HPP
+
+# include <iostream>
+
+#endif
