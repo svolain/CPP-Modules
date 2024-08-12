5c3c8cd766529b3ac024acb02170fcfe89b9bbf6
Ville Savolainen <savoville@gmail.com>
2024-08-12 11:56:44 +0300

    made functions into their own templates

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: vsavolai <vsavolai@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/27 19:17:32 by svolain           #+#    #+#             */
-/*   Updated: 2024/08/12 10:58:31 by vsavolai         ###   ########.fr       */
+/*   Updated: 2024/08/12 11:52:12 by vsavolai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ template <typename T>
 class Array {
     public:
         Array(void);
-        Array(unsigned int n):
-        Array(const Array<T>& src);
+        Array(unsigned int n);
+        Array(Array<T> const &src);
         ~Array(void);
         
         Array<T>&  operator=(const Array<T>& rhs);
@@ -39,7 +39,8 @@ template <typename T>
 Array<T>::Array(unsigned int n):  _size(n), _arr(new T[n]) {}
 
 template <typename T>
-Array<T>::Array(const Array<T>& src): _size(src._size), _arr(new T[src._size]) {
+Array<T>::Array(Array<T> const &src): _size(src._size), _arr(new T[src._size]) 
+{
     for (int i = 0; i < this->_size; ++i) {
         this->_arr[i] = src._arr[i];
     }
@@ -80,7 +81,7 @@ const T& Array<T>::operator[](int index) const
 {
     if (index >= this->_size || index < 0)
         throw std::out_of_range("Index out of range");
-        return this->_arr[index];
+    return this->_arr[index];
 }
 
 template <typename T> 
